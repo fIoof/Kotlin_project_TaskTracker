b59226438f22e1d2aeb9bb06cce40bbd9059f75b
Rebecca Ashington <rashington@conway.services.enablehosts.uk>
2025-12-15 15:48:32 +0000

    Added tracker options for main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,20 @@
 
+import service.TaskService
 
 fun main() {
+    val taskService = service.TaskService()
     println("Welcome to the Task Tracker!")
+
+    while (true) {
+        println(
+            """
+            1. Add Task
+            2. View All Tasks
+            3. Complete Task
+            4. Exit
+            """
+        )
+        
+    }
 
 }
